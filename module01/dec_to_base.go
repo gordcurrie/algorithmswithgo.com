b8d2b37e4b71ba2589eab6306ad7a26914b0886a
Gord Currie <gord@gordcurrie.com>
2022-01-01 17:20:49 -0600

    added fizz buzz and dec to base 10X

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"strconv"
+	"strings"
+)
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -10,5 +15,38 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
+	output := strings.Builder{}
+	more := true
+	for more {
+		remainder := dec % base
+		output.WriteString(convertInt(remainder))
+
+		dec = dec / base
+		if dec == 0 {
+			more = false
+		}
+	}
+
+	return Reverse(output.String())
+}
+
+func convertInt(i int) string {
+	switch i {
+	case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9:
+		return strconv.Itoa(i)
+	case 10:
+		return "A"
+	case 11:
+		return "B"
+	case 12:
+		return "C"
+	case 13:
+		return "D"
+	case 14:
+		return "E"
+	case 15:
+		return "F"
+	}
+
 	return ""
 }
--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -1,5 +1,11 @@
 package module01
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
@@ -11,4 +17,27 @@ package module01
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
+	sb := strings.Builder{}
+	for i := 1; i <= n; i++ {
+		var v string
+
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			v = "Fizz Buzz"
+		case i%3 == 0:
+			v = "Fizz"
+		case i%5 == 0:
+			v = "Buzz"
+		default:
+			v = strconv.Itoa(i)
+		}
+
+		if i == n {
+			sb.WriteString(v)
+		} else {
+			sb.WriteString(fmt.Sprintf("%s, ", v))
+		}
+	}
+
+	fmt.Println(sb.String())
 }
